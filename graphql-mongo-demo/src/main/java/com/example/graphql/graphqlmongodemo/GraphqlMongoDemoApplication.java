b2a95f278e2baf0294e52f8b13f7bfc76a7ffaeb
Miguel <0angel007@gmail.com>
2021-08-17 23:54:58 -0500

    simple graphql app using mongodb and spring boot

---
+++ graphql-mongo-demo/src/main/java/com/example/graphql/graphqlmongodemo/GraphqlMongoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphql.graphqlmongodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlMongoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlMongoDemoApplication.class, args);
+	}
+
+}
